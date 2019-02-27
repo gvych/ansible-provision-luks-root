aee20de876d42d7a9e40415ef139b897b18a2d59
Vyacheslav Gusev <vyacheslav.gusev@gmail.com>
2019-02-27 19:37:12 +0300

    Draft of ssh-key encr for vault.yml

--- password.sh
+++ password.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo 123456
+exit
+
+. encrypted-passwords.txt
+for PASS in ${ENC_PASSWORDS[@]}
+do
+  ssh-keygen -lf ~/.ssh/id_rsa.pub | awk 'NR==1{print $2}'
+  echo $PASS | base64 -d  | openssl rsautl -decrypt -oaep -inkey ~/.ssh/id_rsa |
+  if [[ "$?" -eq 0 ]]; then exit; fi
+done
+
