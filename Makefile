d9a484ede990af6e7bf288440db3126778a3c1e1
Vyacheslav G <vyacheslav.gusev@gmail.com>
2019-08-11 18:24:20 +0300

    Fixes for Vault passwords storing; Draft for password.sh

---
+++ Makefile
@@ -0,0 +1,30 @@
+all:  boot
+
+provision:
+	ansible-playbook --private-key ./ssh-rescure-key provision-in-rescue-mode.yml --skip-tags purge
+
+boot:
+	ansible-playbook -l new open-luks-on-boot.yml
+
+gitlab-config:
+	ansible-playbook -l new ferm-whitelist.yaml
+
+
+kubespray-green:
+	docker run -v ~/.kubespray.yml:/root/.kubespray.yml -v `pwd`/kubespray-green:/root/.kubespray  --entrypoint bash -it --name kubespray-green kubespray
+
+TODO-setup-inside-docker:
+	apt-get update && apt-get install -y python-pip python-dev libssl-dev libffi-dev gcc git
+	pip install --upgrade setuptools ansible-modules-hashivault kubespray -r https://raw.githubusercontent.com/kubernetes-incubator/kubespray/v2.10.4/requirements.txt
+	
+	declare -a IPS=( 10.10.1.3 10.10.1.14 10.10.1.25 10.10.1.36 )
+	kubespray prepare --nodes $(for i in {01..04}; do echo kuber${i}.myproject[ansible_ssh_host=${IPS[$i-1]}];done)
+	
+	cp -a ~/.kubespray/inventory/sample/group_vars  ~/.kubespray/inventory/group_vars
+	sed -i "s/kube_network_plugin:.*/kube_network_plugin: weave/" ~/.kubespray/inventory/group_vars/k8s-cluster/k8s-cluster.yml
+	PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
+	sed -i "s/.*weave_password:.*/weave_password: ${PASSWORD}/" ~/.kubespray/inventory/group_vars/k8s-cluster/k8s-net-weave.yml
+	
+	
+	kubespray deploy
+
--- password.sh
+++ password.sh
@@ -1,6 +1,67 @@
 #!/bin/bash
-echo 123456
+
+shared_password=$( echo 123456 )
+
+
+echo $shared_password
 exit
+
+
+generate-new-password() {
+ 
+  INTERMEDIARYKEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo )
+  rm vault.key.enc
+  #echo $INTERMEDIARYKEY | ./sshcrypt agent-encrypt > vault.key.enc #reinit database with everybodies signatures
+
+  mkdir -p engineers_encrypted_passwords/
+  while read -r ; do  #encrypt new randomkey for each engineer with his public SSH key, so he can access it 
+    fingerprint=$(ssh-keygen -E md5 -lf <(echo $REPLY) | tr ' ()' '___')
+    echo $INTERMEDIARYKEY | openssl rsautl -encrypt -oaep -pubin -inkey <(ssh-keygen -e -f <(echo $REPLY) -m PKCS8) | base64 > engineers_encrypted_passwords/$fingerprint
+  done < engineers_ssh_keys
+  unset INTERMEDIARYKEY
+
+  add-yourself-into-signatures
+}
+
+add-yourself-into-signatures() {
+  fingerprint=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | tr ' ()' '___')
+  KEY=$(< engineers_encrypted_passwords/$fingerprint openssl rsautl -decrypt -oaep -inkey ~/.ssh/id_rsa )
+  echo $KEY | ./sshcrypt agent-encrypt >> vault.key.enc 
+}
+
+
+try-to-open-with-your-signature() {
+ sshcrypt agent-decrypt < vault.key.enc || add-yourself-into-signatures
+}
+
+main func:
+1. check if sshcrypt agent can work
+2. if cant access - get your personal enc by ssh-key
+3. enc personal key using sshcrypt agent
+4. append common list of sshcrypt encs with your
+
+gen new keys:
+1.   gen random passphrase
+1.1. save random passphrase encrypted with agent-encrypt
+1.2. decrypt random passphrase into variable
+2.   for each SSH pub key generate personal enc by ssh-keys
+3.   store in common store of ssh-keys encs 
+4.   go to main func for yourself
+
+
+
+#declare -A encrypted_password
+#shared_password=$(<
+while read -r ; do
+  echo "$enc_pass" > engineers_encrypted_passwords/$fingerprint
+  #fingerprint=$(ssh-keygen -lf $REPLY | awk 'NR==1{print $2}')
+  #encrypted_password["test"]=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo)
+  #encrypted_password["$fingeprint"]=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo)
+< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo | ./sshcrypt agent-encrypt > vault.key.enc
+
+#generate first use with ssh-add password entering for adding youself into vault.key.env
+function add_myself_into_vault.key.enc_using_my_private_key()
+   base64 -d < vault.key.orig | openssl rsautl -decrypt -oaep -inkey ~/.ssh/id_rsa |
 
 . encrypted-passwords.txt
 for PASS in ${ENC_PASSWORDS[@]}
@@ -9,4 +70,3 @@ do
   echo $PASS | base64 -d  | openssl rsautl -decrypt -oaep -inkey ~/.ssh/id_rsa |
   if [[ "$?" -eq 0 ]]; then exit; fi
 done
-
